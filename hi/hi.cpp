179bf8127889197d38cae7d49fe6773bf7cdc70a
dibyojyotiofficial2008 <dibyojyoti.official2008@gmail.com>
2026-07-09 11:28:35 +0530

    Add files via upload

---
+++ hi/hi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    string name = "dibyojyoti";
+    int age = 18;
+    float height = 5.6;
+    bool likecoding = true;
+    cout << "NAME: " << name << endl;
+    cout << "AGE: " << age << endl;
+    cout << "HEIGHT: " << height << endl;
+    cout << "LIKES CODING: " << boolalpha << likecoding << endl;
+    return 0;
+}
